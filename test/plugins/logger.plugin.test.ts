d26efddc2db6174751ccc2e9d4a55cb2aaee32b6
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-08 18:11:48 -0400

    test: ✅ logger plugin test

---
+++ test/plugins/logger.plugin.test.ts
@@ -0,0 +1,31 @@
+import { log } from 'console'
+import { buildLogger, logger as winstonLogger } from '../../src/plugins/logger.plugin'
+
+describe('plugins/loggerPlugin.ts', () => {
+  test('buildLogger should return function logger', () => {
+    const logger = buildLogger('test')
+    expect(logger).toBeDefined()
+    expect(logger.log).toBeDefined()
+    expect(logger.error).toBeDefined()
+  })
+
+  test('logger.log should log a message', () => {
+    //mock
+    const winstonLoggerMock = jest.spyOn(winstonLogger, 'log')
+    const message = 'test message'
+    const service = 'test service'
+    // use plugin
+    const logger = buildLogger(service)
+    logger.log(message)
+
+    //review lock mock
+    expect(winstonLoggerMock).toHaveBeenCalledWith(
+      'info',
+      expect.objectContaining({
+        level: 'info',
+        message,
+        service
+      })
+    )
+  })
+})
