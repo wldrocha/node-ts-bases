f260e4de340645155b5382ce6c7fd8543f0f9192
Wladimir Rocha <wld.rocha@gmail.com>
2024-06-08 12:16:33 -0400

    refactor: 🥅 manage errors by test

--- src/js-foundation/06-promises.ts
+++ src/js-foundation/06-promises.ts
@@ -1,23 +1,23 @@
-const { http } = require('../plugins');
+import { httpClient as http } from '../plugins/'
 
-export const getPokemonById = async( id: string|number ):Promise<string> => {
-  const url = `https://pokeapi.co/api/v2/pokemon/${ id }`;
+export const getPokemonById = async (id: string | number): Promise<string> => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 
-  const pokemon = await http.get( url );
+  try {
+    const pokemon = await http.get(url)
+
+    return pokemon.name
+  } catch (error) {
+    throw `Pokemon not found with id: ${id}`
+  }
 
   // const resp = await fetch( url );
   // const pokemon = await resp.json();
 
-
   // throw new Error('Pokemon no existe');
-  
-  return pokemon.name;
-  
+
   // return fetch( url )
   //   .then( ( resp ) => resp.json())
   //   // .then( () => { throw new Error('Pokemon no existe') })
   //   .then( ( pokemon ) => pokemon.name );
-
 }
-
-
